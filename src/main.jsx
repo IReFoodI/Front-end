53bc8e62bd4da55b8e6fe1c2590a4fcb6150d59d
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-08-18 14:12:55 -0300

    style: :art: Sorting imports using eslint

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
+import "./ui/styles/index.css"
+
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+
 import App from "./app/App.jsx"
-import "./ui/styles/index.css"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
