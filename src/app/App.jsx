e50bd270a020c630cc5f86cc7da30df427ef1e6a
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-08-20 22:12:17 -0300

    chore: :heavy_plus_sign: Intall react-router-dom and tabler/icons

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,6 @@
+import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react"
 import { useState } from "react"
+import { Outlet } from "react-router-dom"
 
 function App() {
   const [state, setState] = useState(0)
@@ -7,13 +9,19 @@ function App() {
     setState((state) => state + val)
   }
   return (
-    <>
+    <div className="flex flex-col gap-4">
       <h1 className="mb-2 flex text-red-500">Hello world</h1>
       <div className="flex items-center gap-3">
-        <button onClick={() => count(-1)}>&lt;</button> <p>{state}</p>
-        <button onClick={() => count(1)}>&gt;</button>
+        <button onClick={() => count(-1)}>
+          <IconArrowLeft size={20} />
+        </button>
+        <p>{state}</p>
+        <button onClick={() => count(1)}>
+          <IconArrowRight size={20} />
+        </button>
       </div>
-    </>
+      <Outlet />
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,20 @@ import "./ui/styles/index.css"
 
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
 import App from "./app/App.jsx"
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [],
+  },
+])
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 )
