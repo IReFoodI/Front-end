fa80541021acd12f47de640f2b93967829962ed3
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-08-24 10:10:19 -0300

    feat: store information tabs

---
+++ src/domains/food/components/pages/storeInformationSectionTabs.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+import { StoreInformationInfo } from "./storeInformationInfo"
+import { StoreInformationReview } from "./storeInformationReview"
+
+export const StoreInformationSectionTabs = () => {
+  const [activeTab, setActiveTab] = useState("info")
+
+  return (
+    <div>
+      <div className="mb-4 flex cursor-pointer border-t border-t-gray-200 text-gray-500">
+        <div
+          onClick={() => setActiveTab("reviews")}
+          className={`w-full px-4 py-2 text-center ${
+            activeTab === "reviews"
+              ? "border-b-2 border-orange-500 font-bold text-orange-500"
+              : ""
+          }`}
+        >
+          Avaliações
+        </div>
+        <div
+          onClick={() => setActiveTab("info")}
+          className={`w-full px-4 py-2 text-center ${
+            activeTab === "info"
+              ? "border-b-2 border-orange-500 font-bold text-orange-500"
+              : ""
+          }`}
+        >
+          Informações
+        </div>
+      </div>
+      <div>
+        {activeTab === "reviews" && <StoreInformationReview />}
+        {activeTab === "info" && <StoreInformationInfo />}
+      </div>
+    </div>
+  )
+}
