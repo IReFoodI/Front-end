84634924288418500055ea8375c9b75f20da57f5
Thasyo Peres <thasyotp2002@gmail.com>
2024-09-03 20:54:05 -0300

    fix: :bug: fixing address modal position

--- src/ui/components/header/addressModal/AddressModal.jsx
+++ src/ui/components/header/addressModal/AddressModal.jsx
@@ -32,7 +32,7 @@ function AddressModal() {
         </div>
       </PopoverTrigger>
       <PopoverContent
-        className="relative top-14 md:absolute md:top-7 md:w-96"
+        className="relative left-4 top-14 md:absolute md:left-0 md:top-5 md:w-96"
         align="start"
       >
         <div className="flex w-full flex-col items-center justify-center gap-2">
