4862e284783f79cfbf29a85a9958e3b4bb29a4ea
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-09-06 15:52:11 -0300

    feat: :construction: add create account route and adjust home
    Added a new route for account creation.
    Adjusted the home route for better layout and structure.
     Routes are still under construction, pending further updates.

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,23 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
-import { Login } from "./domains/user/components/login"
-import { PresentationContent } from "./domains/user/components/loginPresentation/PresentationContent"
+import App from "./app/App"
+import { CreateAccount } from "./domains/user/components/CreateAccount"
+import { InConstruction } from "./domains/user/components/InConstruction"
+import { Login } from "./domains/user/components/Login"
+import { PresentationContent } from "./domains/user/components/PresentationContent"
 import { Layout } from "./ui/layouts/LoginLayout"
 
 const router = createBrowserRouter([
-  // {
-  //   path: "/",
-  //   element: <App />,
-  //   children: [],
-  // },
+  {
+    path: "/home",
+    element: <App />,
+    children: [],
+  },
+  {
+    path: "*",
+    element: <InConstruction />,
+  },
   {
     path: "/",
     element: <Layout />,
@@ -25,6 +32,10 @@ const router = createBrowserRouter([
       {
         path: "/login",
         element: <Login />,
+      },
+      {
+        path: "/create-account",
+        element: <CreateAccount />,
       },
     ],
   },
