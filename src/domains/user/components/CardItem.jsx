80e781823f7c0a65334e36ff2d8a9a1d3d9a4ad9
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-09-13 18:11:50 -0300

    feat: add page myOrders

---
+++ src/domains/user/components/CardItem.jsx
@@ -0,0 +1,64 @@
+/* eslint-disable react/prop-types */
+import {
+  IconCaretRightFilled,
+  IconCheck,
+  IconStarFilled,
+} from "@tabler/icons-react"
+
+function renderStars(rating) {
+  const stars = []
+  for (let i = 0; i < 5; i++) {
+    stars.push(
+      <IconStarFilled
+        key={i}
+        size={15}
+        className={i < rating ? "text-orange-500" : "text-gray-300"}
+      />
+    )
+  }
+  return stars
+}
+export function CardItem({ data }) {
+  return (
+    <div className="w-full min-w-[320px] p-3 text-sm font-semibold">
+      <p className="py-2">{data.date}</p>
+      <div className="flex items-center justify-between rounded-t-lg border-b bg-gray-100 p-2 px-4">
+        <div className="flex gap-3">
+          <img
+            src={data.imgUrl}
+            alt={data.name}
+            className="h-12 w-12 rounded-full object-cover"
+          />
+          <div>
+            <p className="text-xl">{data.name}</p>
+            <p className="text-gray-400">{data.type}</p>
+          </div>
+        </div>
+        <div>
+          <IconCaretRightFilled />
+        </div>
+      </div>
+      <div className="border-b bg-gray-100 p-2 px-4">
+        <div className="flex items-center gap-1">
+          <span className="text-green-500">
+            <IconCheck />
+          </span>
+          Pedido concluído • #{data.orderNumber}
+        </div>
+        <div className="flex items-center justify-between">
+          <p className="text-xl">
+            {data.items}x {data.itemName}
+          </p>
+          <p className="text-orange-500">R$ {data.price.toFixed(2)}</p>
+        </div>
+      </div>
+      <div className="flex items-center justify-between border-b bg-gray-100 p-2 px-4">
+        <p className="">Avalie o pedido</p>
+        <span className="flex gap-2">{renderStars(data.rating)}</span>
+      </div>
+      <div className="rounded-b-lg bg-gray-100 p-2 text-center">
+        <button className="text-orange-500">Ajuda</button>
+      </div>
+    </div>
+  )
+}
---
+++ src/domains/user/components/MyOrdersPage.jsx
@@ -0,0 +1,73 @@
+import { IconShoppingBag } from "@tabler/icons-react"
+
+import { Button } from "./Button"
+import { CardItem } from "./CardItem"
+
+const cardData = [
+  {
+    date: "Ter 06 agosto 2024",
+    name: "Dragão Verde",
+    type: "Restaurante",
+    orderNumber: 3399,
+    items: 1,
+    itemName: "Item a venda",
+    price: 40.0,
+    rating: 4,
+    imgUrl: "https://placehold.co/100x100",
+  },
+  {
+    date: "Qua 07 agosto 2024",
+    name: "Leão Dourado",
+    type: "Pizzaria",
+    orderNumber: 4455,
+    items: 2,
+    itemName: "Pizza grande",
+    price: 80.0,
+    rating: 5,
+    imgUrl: "https://placehold.co/100x100",
+  },
+  {
+    date: "Qui 08 agosto 2024",
+    name: "Lótus Azul",
+    type: "Café",
+    orderNumber: 1122,
+    items: 3,
+    itemName: "Café especial",
+    price: 15.0,
+    rating: 3,
+    imgUrl: "https://placehold.co/100x100",
+  },
+]
+export function MyOrdersPage() {
+  return (
+    <div className="mx-auto flex h-screen w-full max-w-[1216px] flex-col items-center text-gray-600 antialiased lg:h-auto">
+      <h1 className="mt-5 w-full pb-6 pt-7 text-center text-2xl font-semibold lg:py-10">
+        Meus Pedidos
+      </h1>
+      <div className="flex w-full max-w-[320px] flex-col items-center gap-4 text-center">
+        <IconShoppingBag size={45} />
+        <h2 className="text-xl font-semibold">Você ainda não fez pedidos!</h2>
+        <p className="w-[250px]">
+          explore as lojas e tire a barriga da miséria hoje mesmo!
+        </p>
+        <Button>explorar agora!</Button>
+      </div>
+      <div className="flex w-full flex-col items-start px-8">
+        <div className="flex w-full flex-col">
+          <h2 className="my-8 text-center font-semibold text-orange-500 lg:px-24 lg:text-start">
+            Histórico
+          </h2>
+          {cardData.length > 0 ? (
+            <div className="mx-auto grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
+              {cardData.map((data, index) => (
+                <CardItem key={index} data={data} />
+              ))}
+            </div>
+          ) : (
+            <p className="p-4 text-center">Você ainda não fez pedidos.</p>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
