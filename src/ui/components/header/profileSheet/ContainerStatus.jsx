0cb5963c067ecf9c7a04c59966eb1430b53d159e
Thasyo Peres <thasyotp2002@gmail.com>
2024-09-22 14:42:10 -0300

    fix: add fixes in ContainerStatus.jsx

--- src/ui/components/header/profileSheet/ContainerStatus.jsx
+++ src/ui/components/header/profileSheet/ContainerStatus.jsx
@@ -4,11 +4,11 @@ export function ContainerStatus({ containerIcon, content, title }) {
   return (
     <Link
       to="/"
-      className="flex w-full flex-col items-center justify-center rounded-xl bg-gray-200 p-2 text-center hover:bg-orange-100"
+      className="flex w-full flex-col items-center justify-center rounded-xl bg-gray-50 p-2 text-center hover:bg-orange-100"
     >
       {containerIcon}
-      <p className="text-base font-semibold text-orange-700">{content}</p>
-      <p className="text-sm text-zinc-400">{title}</p>
+      <p className="text-base font-semibold text-[#FB3D01]">{content}</p>
+      <p className="text-start text-sm">{title}</p>
     </Link>
   )
 }
