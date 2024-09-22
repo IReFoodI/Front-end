331c174cd8f23a2423c8cd118dab6e6331718503
Thasyo Peres <thasyotp2002@gmail.com>
2024-09-22 14:28:45 -0300

    feat: add DefaultAddress and AddressData components

---
+++ src/ui/components/myDataPage/AddressData.jsx
@@ -0,0 +1,18 @@
+export function AddressData({ address }) {
+  return (
+    <div className="my-2 flex w-[80%] flex-col justify-start">
+      <h2 className="text-xl font-bold text-black">{address.name}</h2>
+      <div className="text-sm">
+        <p>
+          {address.street}, {address.number} - {address.additionalInfo01}
+        </p>
+        <p>
+          {address.additionalInfo02} - {address.neighborhood}
+        </p>
+        <p>
+          {address.city} - {address.state} - CEP {address.zipCode}
+        </p>
+      </div>
+    </div>
+  )
+}
