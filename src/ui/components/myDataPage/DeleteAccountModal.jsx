6ad4c53317eac732b4a7d74ba2d63691d15cb93d
Thasyo Peres <thasyotp2002@gmail.com>
2024-09-22 14:23:38 -0300

    fix: add fixes to DeleteAccountModal

--- src/ui/components/myDataPage/DeleteAccountModal.jsx
+++ src/ui/components/myDataPage/DeleteAccountModal.jsx
@@ -18,14 +18,16 @@ export function DeleteAccountModal() {
     <>
       <Dialog>
         <DialogTrigger asChild>
-          <Link className="underline">Excluir Conta</Link>
+          <Button className="my-12 w-[50%] bg-white text-center text-xl font-semibold text-slate-700 hover:bg-white hover:drop-shadow-md lg:my-5 lg:rounded-full lg:border lg:border-primary lg:p-5 lg:text-primary">
+            <Link>Excluir Conta</Link>
+          </Button>
         </DialogTrigger>
         <DialogContent className="max-w-[90%] rounded-md lg:max-w-[30%]">
           <DialogHeader>
-            <DialogTitle className="w-full text-center">
+            <DialogTitle className="w-full text-center lg:text-2xl">
               Tem certeza que deseja excluir?
             </DialogTitle>
-            <DialogDescription className="m-auto w-full text-center lg:w-[80%]">
+            <DialogDescription className="m-auto w-full text-center lg:w-[90%] lg:text-xl">
               Após a exclusão desta conta todos os seus dados serão apagados.
               Mesmo que crie uma nova conta não será possível recuperar.
             </DialogDescription>
@@ -34,7 +36,7 @@ export function DeleteAccountModal() {
           <DialogFooter>
             <Button
               variant="outline"
-              className="m-auto w-[80%] rounded-full border-2 border-ring text-ring hover:bg-background hover:text-ring hover:drop-shadow-md lg:text-lg"
+              className="m-auto w-[80%] rounded-full border-2 border-ring text-ring hover:bg-background hover:text-ring hover:drop-shadow-md lg:p-5 lg:text-xl"
             >
               Excluir
             </Button>
