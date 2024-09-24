bda370d9085af837f76820ed44c8546ff148eace
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-09-24 15:22:52 -0300

    ✨ feat: add Ilustra image to component
    - Added the Ilustra image to enhance the visual aspect of the existing component.
    - This update improves the user experience by adding a relevant illustration.

--- src/domains/user/components/my-data/MyProfileData.jsx
+++ src/domains/user/components/my-data/MyProfileData.jsx
@@ -16,6 +16,7 @@ import {
 import { Input } from "@/ui/components/ui/input"
 import { PhonePatternFormat } from "@/ui/components/ui/phone-pattern-format"
 
+import ilustra from "../../../../ui/assets/ilustra.png"
 import { ModalCancel } from "./ModalCancel"
 
 //objeto temporário
@@ -57,79 +58,94 @@ export function MyProfileData() {
   })
 
   function onSubmit(data) {
-    console.log(data)
+    console.log(data) // enviar para o back
     toast.success("Informações alteradas com sucesso")
     navigate("/my-profile") //todo: ajustar rota
   }
 
   return (
     <>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="grid gap-2 text-left"
-        >
-          <FormField
-            id="name"
-            name="name"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Nome</FormLabel>
-                <FormControl>
-                  <Input
-                    type="text"
-                    placeholder="Nome completo"
-                    className={"!mt-1"}
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage className={"text-xs"} />
-              </FormItem>
-            )}
-          />
+      <div
+        id="page"
+        className="mx-auto flex h-screen w-full max-w-[1216px] flex-col items-center text-gray-600 antialiased lg:h-auto"
+      >
+        <h1 className="w-full pb-6 pt-7 text-center text-2xl font-semibold lg:py-10">
+          Alterar Dados
+        </h1>
+        <div className="flex w-full">
+          <div className="flex w-full flex-col justify-between px-5 sm:w-1/2">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="grid gap-2 text-left"
+              >
+                <FormField
+                  id="name"
+                  name="name"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Nome</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="text"
+                          placeholder="Nome completo"
+                          className={"!mt-1"}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage className={"text-xs"} />
+                    </FormItem>
+                  )}
+                />
 
-          <FormField
-            id="email"
-            name="email"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input
-                    type="email"
-                    placeholder="Email"
-                    className={"!mt-1"}
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage className={"text-xs"} />
-              </FormItem>
-            )}
-          />
-          <FormField
-            id="phone"
-            name="phone"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Contato</FormLabel>
-                <FormControl>
-                  <PhonePatternFormat {...field} />
-                </FormControl>
-                <FormMessage className={"text-xs"} />
-              </FormItem>
-            )}
-          />
-          <div className="mt-4 grid gap-3 sm:grid-cols-2 sm:gap-5">
-            <Button className="order-2" type="submit">
-              Salvar
-            </Button>
-            <ModalCancel />
+                <FormField
+                  id="email"
+                  name="email"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="email"
+                          placeholder="Email"
+                          className={"!mt-1"}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage className={"text-xs"} />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  id="phone"
+                  name="phone"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Contato</FormLabel>
+                      <FormControl>
+                        <PhonePatternFormat {...field} />
+                      </FormControl>
+                      <FormMessage className={"text-xs"} />
+                    </FormItem>
+                  )}
+                />
+                <div className="mt-4 grid gap-3 sm:grid-cols-2 sm:gap-5">
+                  <Button className="order-2" type="submit">
+                    Salvar
+                  </Button>
+                  <ModalCancel />
+                </div>
+              </form>
+            </Form>
           </div>
-        </form>
-      </Form>
+          <div className="hidden sm:flex">
+            <img className="max-h-[700px]" src={ilustra} alt="" />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
