068db58c26c6fc80f97bfaf51968eb11b604186c
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-09-24 15:19:06 -0300

    💄 style: remove sm:max-w-[50%] from input class
    - Removed the `sm:max-w-[50%]` style from the input class to adjust the input width behavior across screen sizes.
    - This ensures the input field occupies the full width as intended on smaller screens.

--- src/ui/components/ui/phone-pattern-format.jsx
+++ src/ui/components/ui/phone-pattern-format.jsx
@@ -8,11 +8,11 @@ export const PhonePatternFormat = forwardRef((props, ref) => (
     allowEmptyFormatting 
     mask="_" 
     getInputRef={ref}
-    className="input !mt-1 flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 sm:max-w-[50%]"
+    className="input !mt-1 flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
   />
 ));
 
-// Definir o displayName para melhorar a depuração
+
 PhonePatternFormat.displayName = 'PhonePatternFormat';
 
 
