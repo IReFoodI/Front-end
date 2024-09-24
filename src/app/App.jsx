c0bd7be8ec40e72a036a529eba8663d6be4be7a1
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-09-24 11:58:59 -0300

    🔄 chore: remove Header component from App
    - Removed the `Header` component from the `App` component.
    - This change prepares for potential redesign or re-evaluation of the header structure in the application.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,9 @@
 import { Outlet } from "react-router-dom"
 
-import Header from "@/ui/components/header/Header"
-
 function App() {
   return (
     <>
       <Outlet />
-      <Header />
     </>
   )
 }
