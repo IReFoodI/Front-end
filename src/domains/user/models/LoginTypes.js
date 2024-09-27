e7cc6c81269ca65c524b5d793aa00abc9a2191d6
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-09-27 17:21:38 -0300

    ✨ feat: add validation schema for login
    - Created loginTypes.js to define the validation schema for user login.
    - Utilized Zod for schema validation, ensuring input fields (email and password) meet specified requirements.

---
+++ src/domains/user/models/LoginTypes.js
@@ -0,0 +1,12 @@
+import { z } from "zod"
+
+export const formSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "Campo obrigatório" })
+    .email({ message: "Formato de e-mail inválido" }),
+  password: z
+    .string()
+    .min(1, { message: "Campo obrigatório" })
+    .min(6, { message: "A senha deve ter pelo menos 6 caracteres" }),
+})
