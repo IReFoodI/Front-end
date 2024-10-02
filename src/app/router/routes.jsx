bd23f7b7b7e42342153905dd3cabe1cac56c31dc
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-10-02 20:23:04 -0300

    refactor: :recycle: Update PageNotFound component

--- src/app/router/routes.jsx
+++ src/app/router/routes.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom"
 
 import { AddressPage } from "@/domains/user/components/AddressPage.jsx"
-import { PageNotFound } from "@/domains/user/components/PageNotFound.jsx"
+import { PageNotFound } from "@/ui/components/PageNotFound.jsx"
 
 import App from "../App.jsx"
 
