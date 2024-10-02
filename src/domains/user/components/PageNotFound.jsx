5b05f33bbc121d5f0cddff128b5cc5acc460ea82
Felipe Fracasso <93887208+FelipeM-F@users.noreply.github.com>
2024-10-02 18:55:10 -0400

    style: :lipstick: Mudança do componente InConstruction para PageNotFound

--- src/domains/user/components/PageNotFound.jsx
+++ src/domains/user/components/PageNotFound.jsx
@@ -1,18 +1,19 @@
-import { Link } from "react-router-dom";
-import imagem from "@/ui/assets/page404.png";
+import { Link } from "react-router-dom"
+
+import imagem from "@/ui/assets/page404.png"
 
 export function PageNotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center">
-      <img src={imagem} alt="404 Not Found" className="w-2/3 mb-6" />
-      <h2 className="text-inherit mb-4">Página não encontrada</h2>
+    <div className="flex h-screen flex-col items-center justify-center text-center">
+      <img src={imagem} alt="404 Not Found" className="mb-6 w-2/3" />
+      <h2 className="mb-4 text-inherit">Página não encontrada</h2>
 
-      <Link 
-        to="/" 
-        className="text-primary underline text-lg hover:text-orange-600"
+      <Link
+        to="/"
+        className="text-lg text-primary underline hover:text-orange-600"
       >
         Voltar para home
       </Link>
     </div>
-  );
+  )
 }
