d435667bf9416f08c0a2ac4c5ae31cdd9d3035cb
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-10-03 18:23:48 -0300

    feat: :sparkles: Add initial DashboardLayout

--- src/app/router/routes.jsx
+++ src/app/router/routes.jsx
@@ -1,20 +1,32 @@
 import { createBrowserRouter } from "react-router-dom"
 
+import { FinancePage } from "@/domains/store/dashboard/FinancePage.jsx"
 import { AddressPage } from "@/domains/user/components/AddressPage.jsx"
 import { PageNotFound } from "@/ui/components/PageNotFound.jsx"
+import { DashBoardLayout } from "@/ui/layouts/DashboardLayout.jsx"
 
 import App from "../App.jsx"
 
 export const ROUTES = {
   ADDRESS: "/address",
+  FINANCAS: "financas",
 }
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: ROUTES.ADDRESS, element: <AddressPage /> }],
+    children: [
+      {
+        path: "dashboard",
+        element: <DashBoardLayout />,
+        children: [{ path: ROUTES.FINANCAS, element: <FinancePage /> }],
+      },
+
+      { path: ROUTES.ADDRESS, element: <AddressPage /> },
+    ],
   },
+
   {
     path: "*",
     element: <PageNotFound />,
