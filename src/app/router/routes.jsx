dc3f6896467a41b123fa7b8ff06c32fb334fc45c
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-10-06 18:44:56 -0300

    🔧 fix: adjusted routes to include login
    - Updated the routes file to include a new route for the login page.
    - The login route was added to enable users to authenticate before accessing protected sections of the application.
    - Ensured that the new login path is correctly registered within the routing configuration.

--- src/app/router/routes.jsx
+++ src/app/router/routes.jsx
@@ -1,26 +1,66 @@
 import { createBrowserRouter } from "react-router-dom"
 
 import { AddressPage } from "@/domains/user/components/AddressPage.jsx"
+import { CreateAccount } from "@/domains/user/components/login/CreateAccount.jsx"
+import { Login } from "@/domains/user/components/login/Login.jsx"
+import { PresentationContent } from "@/domains/user/components/login/PresentationContent.jsx"
 import { ChangePassword } from "@/domains/user/components/password/ChangePassword.jsx"
 import { PageNotFound } from "@/ui/components/PageNotFound.jsx"
+import { Layout } from "@/ui/layouts/LoginLayout.jsx"
 
 import App from "../App.jsx"
+import { ProtectedRoute } from "./ProtectedRoute.jsx"
 
 export const ROUTES = {
-  ADDRESS: "/address",
-  CHANGE_PASSWORD: "/alterar-senha",
+  ADDRESS: "address",
+  CHANGE_PASSWORD: "alterar-senha",
+  LOGIN: "/entrar",
+  CREATE_ACCOUNT: "/criar-conta",
 }
+
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/home",
     element: <App />,
     children: [
-      { path: ROUTES.ADDRESS, element: <AddressPage /> },
-      { path: ROUTES.CHANGE_PASSWORD, element: <ChangePassword /> },
+      {
+        path: ROUTES.ADDRESS,
+        element: (
+          <ProtectedRoute>
+            <AddressPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: ROUTES.CHANGE_PASSWORD,
+        element: (
+          <ProtectedRoute>
+            <ChangePassword />
+          </ProtectedRoute>
+        ),
+      },
     ],
   },
   {
     path: "*",
     element: <PageNotFound />,
+  },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <PresentationContent />,
+      },
+      {
+        path: ROUTES.LOGIN,
+        element: <Login />,
+      },
+      {
+        path: ROUTES.CREATE_ACCOUNT,
+        element: <CreateAccount />,
+      },
+    ],
   },
 ])
