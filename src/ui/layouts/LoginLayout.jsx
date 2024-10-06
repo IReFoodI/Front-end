9606740ad1119986ceb6aecc6fcb12ffc0b1b8c6
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-10-06 19:21:55 -0300

    🔧 refactor: remove LoginProvider and update CSS in Layout component
    - Removed the LoginProvider from the Layout component to simplify context management.
    - Adjusted CSS styles for improved layout and responsiveness:
      - Implemented a grid layout to enhance the structure of the page.
      - Updated padding and margins for better spacing on different screen sizes.
      - Set overflow properties to ensure proper scrolling behavior in the content area.
    - The Decorative component is now conditionally hidden to streamline the layout.

--- src/ui/layouts/LoginLayout.jsx
+++ src/ui/layouts/LoginLayout.jsx
@@ -1,18 +1,14 @@
 import { Outlet } from "react-router-dom"
 
-import { LoginProvider } from "@/app/context/LoginContext"
-
 import { Decorative } from "../../domains/user/components/login/Decorative"
 
 export function Layout() {
   return (
-    <div className="grid h-screen content-center items-center p-3 sm:grid-cols-2 lg:px-0">
-      <LoginProvider>
-        <Decorative className={"hidden"} />
-        <div className="grid max-w-lg content-center gap-2 bg-background text-center sm:px-16 sm:py-12">
-          <Outlet />
-        </div>
-      </LoginProvider>
+    <div className="grid h-screen items-center p-3 sm:grid-cols-2 lg:px-0">
+      <Decorative className={"hidden"} />
+      <div className="grid max-h-dvh gap-2 overflow-y-auto overflow-x-hidden bg-background text-center sm:px-16 sm:py-12">
+        <Outlet />
+      </div>
     </div>
   )
 }
