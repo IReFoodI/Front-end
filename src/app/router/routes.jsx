9a48f6e0d20eee9111fa157d09ad1c6b8c0562e6
Samilis Brito <samilisbritto@gmail.com>
2024-10-07 14:59:29 -0300

    ✨ feat: add route for StoreProfilePage component
    - Integrated the StoreProfilePage component into the main routing structure.
    - Added route to access StoreProfilePage at /store-profile.
    - Ensured that the component loads correctly within the application context.

--- src/app/router/routes.jsx
+++ src/app/router/routes.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom"
 
+import { StoreProfilePage } from "@/domains/store/dashboard/StoreProfilePage.jsx"
 import { AddressPage } from "@/domains/user/components/AddressPage.jsx"
 import { PageNotFound } from "@/ui/components/PageNotFound.jsx"
 
@@ -7,13 +8,17 @@ import App from "../App.jsx"
 
 export const ROUTES = {
   ADDRESS: "/address",
+  DASHBOARD: "dashboard",
 }
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: ROUTES.ADDRESS, element: <AddressPage /> }],
+    children: [
+      { path: ROUTES.ADDRESS, element: <AddressPage /> },
+      { path: ROUTES.DASHBOARD, element: <StoreProfilePage /> },
+    ],
   },
   {
     path: "*",
