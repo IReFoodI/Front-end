a2146c8da8042716ebd4f2660d50d405b1059282
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-10-08 13:34:07 -0300

    feat: :sparkles: Add initial google oauth

--- src/domains/user/components/login/SocialAuthButtons.jsx
+++ src/domains/user/components/login/SocialAuthButtons.jsx
@@ -1,16 +1,58 @@
+import { GoogleLogin, useGoogleLogin } from "@react-oauth/google"
+import axios from "axios"
+
 import facebook from "@/ui/assets/facebook-icon.svg"
 import gmail from "@/ui/assets/gmail-icon.svg"
 import { Button } from "@/ui/components/ui/button/button"
 
 export function SocialAuthButtons() {
+  const login = useGoogleLogin({
+    onSuccess: async (credetialResponse) => {
+      console.log(credetialResponse)
+
+      try {
+        const response = await axios.get(
+          "https://www.googleapis.com/oauth2/v3/userinfo",
+          {
+            headers: {
+              Authorization: `Bearer ${credetialResponse.access_token}`,
+            },
+          }
+        )
+        console.log(response)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+
+    onError: (error) => {
+      console.log("error")
+      console.log(error)
+    },
+  })
+
   return (
     <>
       <span className="text-xs text-muted-foreground">ou</span>
       <nav aria-label="Opções de login e registro" className="grid gap-2">
-        <Button variant="outline">
+        <Button
+          variant="outline"
+          onClick={() => {
+            login()
+          }}
+        >
           <img src={gmail} alt="icone do gmail" className="mr-2 size-4" />
           Continuar com Google
         </Button>
+
+        <GoogleLogin
+          onSuccess={(credetialResponse) => {
+            console.log(credetialResponse)
+          }}
+          onError={(error) => {
+            console.log(error)
+          }}
+        ></GoogleLogin>
         <Button variant="outline">
           <img src={facebook} alt="icone do facebook" className="mr-2 size-4" />
           Continuar com Facebook
