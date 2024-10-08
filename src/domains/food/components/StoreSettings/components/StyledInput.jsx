5a97327732eebb374648943a8cf01b956b18272c
Yasmin-Carloto <yasmincarlotobezerra17@gmail.com>
2024-10-08 13:48:15 -0300

    feat: adding textarea as an option for StyledInput

--- src/domains/food/components/StoreSettings/components/StyledInput.jsx
+++ src/domains/food/components/StoreSettings/components/StyledInput.jsx
@@ -1,17 +1,26 @@
 export function StyledInput({
   isDisabled,
+  isTextArea,
   onInputChange,
   inputName,
   inputValue,
   inputPlaceholder,
 }) {
-  return (
+  return isTextArea ? (
+    <textarea
+      name={inputName}
+      onChange={(event) => onInputChange(event)}
+      className={`w-full rounded-md border border-zinc-400 ${isDisabled ? "bg-slate-200" : "bg-none"} h-32 resize-none p-2 outline-orange-500`}
+      placeholder={inputPlaceholder}
+      value={inputValue}
+    ></textarea>
+  ) : (
     <input
       type="text"
       disabled={isDisabled}
       onChange={(event) => onInputChange(event)}
       name={inputName}
-      className={`w-full rounded-md border border-zinc-400 ${isDisabled ? "bg-slate-200" : "bg-none"} p-2 outline-orange-500`}
+      className={`w-full rounded-md border border-zinc-400 ${isDisabled ? "bg-slate-200" : "bg-none"} p-2 outline-orange-500 ${isTextArea ? "h-32" : ""}`}
       value={inputValue}
       placeholder={inputPlaceholder}
     />
