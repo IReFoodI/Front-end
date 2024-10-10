be6141fa23e107f43a3a86d99c870be61e36f56c
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-10-10 19:32:51 -0300

    feat: :sparkles: Add Dashboard configuration page component

---
+++ src/domains/store/dashboard/configuration-page/ConfigurationPage.jsx
@@ -0,0 +1,42 @@
+import { Separator } from "@/ui/components/ui/separator"
+
+import { ChangeEmailForm } from "./ChangeEmailForm"
+import { ChangePasswordForm } from "./ChangePasswordForm"
+
+export function ConfigurationPage() {
+  const currentEmail = "johndoe@example.com"
+
+  return (
+    <div className="flex w-full max-w-screen-2xl flex-col items-center justify-start gap-6 p-4">
+      <div className="flex w-full flex-col items-center justify-center gap-2">
+        <h1 className="text-2xl font-semibold text-foreground md:text-3xl">
+          Configurações
+        </h1>
+        <h3 className="text-lg font-medium text-zinc-500">
+          Ajuste as informações de senha e e-mail
+        </h3>
+      </div>
+      <div className="flex w-full flex-col items-center justify-start gap-6 lg:flex-row lg:gap-12">
+        <section className="flex w-full flex-col gap-2 lg:h-full lg:justify-between">
+          <div className="flex flex-col">
+            <p className="text-lg font-semibold">E-mail</p>
+            <Separator />
+          </div>
+          <p className="items-start text-sm text-zinc-800 lg:flex-1">
+            E-mail atual: <span className="text-primary">{currentEmail}</span>
+          </p>
+          <ChangeEmailForm />
+        </section>
+
+        <section className="flex w-full flex-col gap-2">
+          <div className="flex flex-col">
+            <p className="text-lg font-semibold">Senha</p>
+            <Separator />
+          </div>
+
+          <ChangePasswordForm />
+        </section>
+      </div>
+    </div>
+  )
+}
