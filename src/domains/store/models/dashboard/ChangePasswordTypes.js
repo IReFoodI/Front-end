cc54a1bacf1cd97be05a58ab3e2c45d2d0f3ba6c
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-10-10 19:31:21 -0300

    feat: :label: Add change password types

---
+++ src/domains/store/models/dashboard/ChangePasswordTypes.js
@@ -0,0 +1,45 @@
+import { z } from "zod"
+
+export const changePasswordTypes = z
+  .object({
+    oldPassword: z
+      .string()
+      .min(1, { message: "Campo obrigatório" })
+      .min(8, { message: "A senha deve ter pelo menos 8 caracteres" })
+      .regex(/^\S*$/, { message: "A senha não deve conter espaços" })
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        {
+          message:
+            "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial",
+        }
+      ),
+    password: z
+      .string()
+      .min(1, { message: "Campo obrigatório" })
+      .min(8, { message: "A senha deve ter pelo menos 8 caracteres" })
+      .regex(/^\S*$/, { message: "A senha não deve conter espaços" })
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        {
+          message:
+            "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial",
+        }
+      ),
+    confirmPassword: z
+      .string()
+      .min(1, { message: "Campo obrigatório" })
+      .min(8, { message: "A senha deve ter pelo menos 8 caracteres" })
+      .regex(/^\S*$/, { message: "A senha não deve conter espaços" })
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        {
+          message:
+            "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial",
+        }
+      ),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "As senhas não coincidem",
+    path: ["confirmPassword"],
+  })
