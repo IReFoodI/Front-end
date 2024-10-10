c9db0de22bbb367ec2d77fabb819c9e2adeaf9f3
Yasmin-Carloto <yasmincarlotobezerra17@gmail.com>
2024-10-10 20:34:51 -0300

    using react form hook with zodResolver for validations

---
+++ src/domains/store/models/StoreAccountTypes.js
@@ -0,0 +1,25 @@
+import { z } from "zod"
+
+const CNPJRegex = /^\d{2}\.?\d{3}\.?\d{3}\/?\d{4}-?\d{2}$/
+const phoneRegex = /^\(?\d{2}\)?\s?\d{5}-?\d{4}$/
+
+export const storeFormSchema = z.object({
+  storeName: z
+    .string({
+      required_error: "Campo obrigatório!",
+    })
+    .trim()
+    .min(1, { message: "Campo obrigatório!" })
+    .max(200, { message: "Você ultrapassou o limite de 200 caracteres!" }),
+  storeCNPJ: z.string().trim().regex(CNPJRegex, { message: "CNPJ inválido!" }),
+  storePhone: z
+    .string({
+      required_error: "Campo obrigatório!",
+    })
+    .trim()
+    .min(1, { message: "Campo obrigatório!" })
+    .regex(phoneRegex, { message: "Telefone inválido!" }),
+  storeDescription: z
+    .string()
+    .min(20, { message: "Campo descrição deve ter no mínimo 20 caracteres." }),
+})
