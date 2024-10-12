3ae20a783d4d45750e228b266eeaf75432ad34d7
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-10-12 19:41:42 -0300

    feat: :sparkles: Remove header from app and add userLayout

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,8 @@
 import { Outlet } from "react-router-dom"
 
-import { Header } from "@/ui/components/header/Header"
-
 function App() {
   return (
     <>
-      <Header />
       <Outlet />
     </>
   )
