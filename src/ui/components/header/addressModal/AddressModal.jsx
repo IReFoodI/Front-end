4372cdd8092db15fb157e1933d3fbf4dac2ceaac
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-10-12 19:32:36 -0300

    feat: :sparkles: Add meus endereços link

--- src/ui/components/header/addressModal/AddressModal.jsx
+++ src/ui/components/header/addressModal/AddressModal.jsx
@@ -53,7 +53,7 @@ export function AddressModal() {
           </div>
         </div>
         <Button className="w-full rounded-xl md:text-xl">
-          <Link to="/" className="w-full">
+          <Link to="/address" className="w-full">
             Meus Endereços
           </Link>
         </Button>
