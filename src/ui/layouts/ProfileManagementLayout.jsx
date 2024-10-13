8b0ece56536a40091112f40f8ad32971b7df886a
Samilis Brito <samilisbritto@gmail.com>
2024-10-13 16:53:59 -0300

    ♻️ refactor: move layout width constraints to UserLayout
    - Removed `mx-auto`, `max-w-screen-2xl`, `md:w-[85%]`, and `lg:w-[70%]` from `ProfileManagementLayout` and applied them in `UserLayout` to standardize the layout across all user screens.

--- src/ui/layouts/ProfileManagementLayout.jsx
+++ src/ui/layouts/ProfileManagementLayout.jsx
@@ -6,7 +6,7 @@ export function ProfileManagementLayout() {
   return (
     <div
       id="page"
-      className="mx-auto h-screen max-h-full max-w-screen-2xl items-start text-gray-600 antialiased md:w-[85%] lg:grid lg:h-auto lg:w-[70%] lg:grid-cols-2 lg:gap-x-8"
+      className="h-screen max-h-full items-start text-gray-600 antialiased lg:grid lg:h-auto lg:grid-cols-2 lg:gap-x-8"
     >
       <Outlet />
       <div className="hidden lg:grid">
--- src/ui/layouts/UserLayout.jsx
+++ src/ui/layouts/UserLayout.jsx
@@ -6,7 +6,9 @@ export function UserLayout() {
   return (
     <>
       <Header />
-      <Outlet />
+      <div className="mx-auto max-w-screen-2xl p-3 md:w-[85%] lg:w-[70%] lg:p-0">
+        <Outlet />
+      </div>
     </>
   )
 }
