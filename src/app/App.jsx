412abb9dc5bcf209bd5e3ff84dac2251dc5cf1e0
Yasmin-Carloto <yasmincarlotobezerra17@gmail.com>
2024-10-13 11:23:44 -0300

    refactor: removing unnecessary use of component StoreProfileSettings.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,9 @@
 import { Outlet } from "react-router-dom"
 
-import { StoreProfileSettings } from "@/domains/food/components/StoreSettings/StoreProfileSettings"
-
 function App() {
   return (
     <>
       <Outlet />
-      <StoreProfileSettings />
     </>
   )
 }
