7fa778fa5f3742617d472b2b1f6c6c9c8b515c18
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-10-23 10:59:47 -0300

    refactor: :recycle: user store

--- src/app/hooks/useStoreUser.js
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand"
-
-export const useStoreUser = create((set) => ({
-  user: {},
-  addUser: (data) => set({ user: data }),
-  removeUser: () => set({ user: {} }),
-}))
