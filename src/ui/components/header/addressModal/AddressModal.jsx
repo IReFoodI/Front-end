3e531a9e89d495da961a53ff752123bc02af02db
Samilis Brito <samilisbritto@gmail.com>
2024-10-24 00:46:29 -0300

    🔗 feat: integrate AddressModal with backend

--- src/ui/components/header/addressModal/AddressModal.jsx
+++ src/ui/components/header/addressModal/AddressModal.jsx
@@ -2,26 +2,42 @@ import { IconCaretDownFilled } from "@tabler/icons-react"
 import { useState } from "react"
 import { Link } from "react-router-dom"
 
+import useFetch from "@/app/hooks/useFetch"
+import { addressService } from "@/domains/user/services/addressService"
+
 import { Button } from "../../ui/button/button"
 import { Popover, PopoverContent, PopoverTrigger } from "../../ui/popover"
 
 export function AddressModal() {
+  const formatCep = (Cep) => {
+    return Cep?.replace(/(\d{5})(\d{3})/, "$1-$2")
+  }
   const [isActive, setIsActive] = useState(false)
+  const [defaultAddress, setDefaultAddress] = useState()
+  const { onRequest } = useFetch()
+  async function fetchAddressDefault() {
+    await onRequest({
+      request: () => addressService.getAddressDefault(),
+      onSuccess: (data) => setDefaultAddress(data),
+    })
+  }
+
+  console.log("de", defaultAddress)
 
   function handleClick() {
-    setIsActive(true)
+    setIsActive(!isActive)
+    fetchAddressDefault()
   }
 
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <div
+        <Button
+          variant="ghost"
           className={`order-1 m-2 flex w-full cursor-pointer items-center justify-center rounded-xl p-2 hover:bg-orange-100 md:order-2 md:w-fit ${isActive ? "active:bg-orange-100" : ""}`}
           onClick={handleClick}
         >
-          <p className="text-sm font-semibold md:text-[15px]">
-            Endereço da pessoa, XX
-          </p>
+          <p className="text-sm font-semibold md:text-base">Endereço</p>
           <span>
             <IconCaretDownFilled
               stroke={2}
@@ -29,32 +45,55 @@ export function AddressModal() {
               size={20}
             />
           </span>
-        </div>
+        </Button>
       </PopoverTrigger>
       <PopoverContent
         className="relative top-14 md:absolute md:left-0 md:top-5 md:w-96"
         align="start"
       >
         <div className="flex w-full flex-col items-center justify-center gap-2">
-          <h1 className="text-[15px] font-bold md:text-xl">
-            Seu pedido irá para esse endereço
+          <h1 className="text-base font-bold md:text-xl">
+            {defaultAddress ? "Endereço padrão" : "Cadastre um endereço"}
           </h1>
-          <p className="text-center text-[12px] text-[#898CA4] md:text-[15px]">
-            As opções e velocidade de entrega podem variar de acordo com a
-            região
+          <p className="text-center text-xs text-[#898CA4] md:text-base">
+            {defaultAddress
+              ? "Seu endereço padrão ajuda na sua experiência"
+              : "Adicione um endereço padrão para personalizar sua experiência"}
           </p>
         </div>
-        <div className="my-6 flex w-full flex-col items-start justify-center gap-2 rounded-lg bg-[#F8F9FE] p-2">
-          <h1 className="text-[15px] font-bold md:text-xl">Casa</h1>
-          <div className="text-sm text-[#616375] md:text-[15px]">
-            <p>Av Vamo pra Cima, 10 - Apto 02</p>
-            <p>Bloco A - Bairro Cruzes</p>
-            <p>Não-me-Toque - RS - CEP XX.XXX-XX</p>
+        {defaultAddress ? (
+          <div className="my-6 flex w-full flex-col items-start justify-center gap-2 rounded-lg bg-secondary p-2">
+            <h1 className="text-base font-semibold text-primary md:text-xl">
+              {" "}
+              {defaultAddress.type}
+            </h1>
+            <div>
+              <p>
+                {defaultAddress.street}, {defaultAddress.number}
+              </p>
+
+              {defaultAddress.complement && <p>{defaultAddress.complement}</p>}
+              <p>{defaultAddress.district}</p>
+              <p>
+                {defaultAddress.city} - {defaultAddress.state} - CEP{" "}
+                {formatCep(defaultAddress.cep)}
+              </p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="my-6 rounded-lg bg-secondary p-2 text-center">
+            <p className="text-base font-semibold text-primary md:text-xl">
+              Você ainda não possue endereço padrão!
+            </p>
+          </div>
+        )}
+
         <Button className="w-full rounded-xl md:text-xl">
-          <Link to="/endereco" className="w-full">
-            Meus Endereços
+          <Link
+            to={defaultAddress ? "/endereco" : "/endereco/adicionar"}
+            className="w-full sm:text-lg"
+          >
+            {defaultAddress ? "Meus Endereços" : "Adicionar"}
           </Link>
         </Button>
       </PopoverContent>
