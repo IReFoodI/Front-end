9b9b0a63a08b453d9dfdb51a520ea1e9bbc14939
Samilis Brito <samilisbritto@gmail.com>
2024-10-24 00:13:17 -0300

    ✨ feat: add address management functions in updateAddress
    - Added new async functions to manage address operations in updateAddress:
      - getAddressById(id): Fetches address details by ID.
      - deleteAddressById(id): Deletes the address with the given ID.
      - patchAddressById(id): Updates the address with the specified ID.
      - getAddressDefault(): Retrieves the default address for the user.

--- src/domains/user/services/addressService.js
+++ src/domains/user/services/addressService.js
@@ -1,7 +1,6 @@
 import { createAxiosInstance } from "@/app/service/createAxiosInstace"
 
 const BASE_URL = "/api/address"
-const axios = createAxiosInstance(true)
 
 /**
  * Create address
@@ -14,14 +13,57 @@ const axios = createAxiosInstance(true)
  * @param {string} data.number - The street number.
  * @param {string} data.complement - Any additional address information (e.g., apartment number).
  * @param {string} data.addressType - The type of address (e.g., home, work).
- * @param {boolean} data.isStandard - Indicates if this is a standard address.
  */
 async function createAddress(data) {
+  const axios = createAxiosInstance(true)
   return await axios.post(BASE_URL, data)
 }
 
+/**
+ * Update address
+ *
+ * @param {Object} data - The address data object.
+ * @param {number} data.addressId - The ID of the address.
+ * @param {string} data.cep - The postal code of the address.
+ * @param {string} data.state - The state abbreviation.
+ * @param {string} data.district - The district of the address.
+ * @param {string} data.street - The street name.
+ * @param {string} data.number - The street number.
+ * @param {string} data.complement - Any additional address information (e.g., apartment number).
+ * @param {string} data.addressType - The type of address (e.g., home, work).
+ */
+async function updateAddress(data) {
+  const axios = createAxiosInstance(true)
+  return await axios.put(`${BASE_URL}/${data.addressId}`, data)
+}
+
 async function listAddresses() {
+  const axios = createAxiosInstance(true)
   return await axios.get(`${BASE_URL}/user`)
 }
 
-export const addressService = { createAddress, listAddresses }
+async function getAddressById(id) {
+  const axios = createAxiosInstance(true)
+  return await axios.get(`${BASE_URL}/me/${id}`)
+}
+async function deleteAddressById(id) {
+  const axios = createAxiosInstance(true)
+  return await axios.delete(`${BASE_URL}/${id}`)
+}
+async function patchAddressById(id) {
+  const axios = createAxiosInstance(true)
+  return await axios.patch(`${BASE_URL}/${id}`)
+}
+async function getAddressDefault() {
+  const axios = createAxiosInstance(true)
+  return await axios.get(`${BASE_URL}/default`)
+}
+export const addressService = {
+  createAddress,
+  listAddresses,
+  getAddressById,
+  updateAddress,
+  deleteAddressById,
+  patchAddressById,
+  getAddressDefault,
+}
