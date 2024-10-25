bdb1a80f7613783719bf02715ffd8be8377415de
Samilis Brito <samilisbritto@gmail.com>
2024-10-25 18:07:55 -0300

    🧹 refactor: remove redundant stats state from changeUserAddressTypes
    - Removed the `stats` state from the `changeUserAddressTypes` component, as it is already managed elsewhere in the application.

--- src/domains/user/models/ChangeUserAddressTypes.js
+++ src/domains/user/models/ChangeUserAddressTypes.js
@@ -1,34 +1,6 @@
 import { z } from "zod"
 
-export const states = [
-  "AC",
-  "AL",
-  "AP",
-  "AM",
-  "BA",
-  "CE",
-  "DF",
-  "ES",
-  "GO",
-  "MA",
-  "MT",
-  "MS",
-  "MG",
-  "PA",
-  "PB",
-  "PR",
-  "PE",
-  "PI",
-  "RJ",
-  "RN",
-  "RS",
-  "RO",
-  "RR",
-  "SC",
-  "SP",
-  "SE",
-  "TO",
-]
+import { states } from "@/domains/store/models/StoreAddressType"
 
 export const changeUserAddressTypes = z.object({
   zipCode: z
