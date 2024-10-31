f87117ff26f4a48f8d4567bdeeb6658eca75caba
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-10-31 15:07:00 -0300

    feat: :sparkles: Add searchParams to RestaurantFilter

--- src/ui/components/header/restaurantFilter/RestaurantFilter.jsx
+++ src/ui/components/header/restaurantFilter/RestaurantFilter.jsx
@@ -1,7 +1,15 @@
 import { Cross1Icon } from "@radix-ui/react-icons"
 import { PopoverClose } from "@radix-ui/react-popover"
-import { IconMapPin } from "@tabler/icons-react"
+import {
+  IconBaguette,
+  IconBuildingStore,
+  IconBurger,
+  IconCandy,
+  IconChefHat,
+  IconSalt,
+} from "@tabler/icons-react"
 import { useState } from "react"
+import { useSearchParams } from "react-router-dom"
 
 import { Slider } from "@/ui/components/ui/slider"
 
@@ -9,28 +17,57 @@ import { FilterItemContainer } from "./FilterItemContainer"
 import { SliderMetric } from "./SliderMetric"
 
 export function RestaurantFilter() {
-  const [sliderValue, setSlidervalue] = useState([0])
+  const [searchParams, setSearchParams] = useSearchParams()
+  const [sliderValue, setSlidervalue] = useState(() => {
+    const param = searchParams?.get("preco")
+    if (param) return [Number(param)]
+    return [0]
+  })
 
-  // const ordinateBy = [
-  //   {
-  //     buttonTitle: "Mais próximos",
-  //     imageSource: <IconMapPin className="size-4" />,
-  //   },
-  //   {
-  //     buttonTitle: "Mais próximos",
-  //     imageSource: <IconStar className="size-4" />,
-  //   },
-  // ]
+  const storeType = [
+    {
+      buttonTitle: "Restaurante",
+      imageSource: <IconChefHat className="size-4" />,
+    },
+    {
+      buttonTitle: "Padaria",
+      imageSource: <IconBaguette className="size-4" />,
+    },
+    {
+      buttonTitle: "Supermercado",
+      imageSource: <IconBuildingStore className="size-4" />,
+    },
+    {
+      buttonTitle: "Lancheria",
+      imageSource: <IconBurger className="size-4" />,
+    },
+  ]
 
-  // const kindOfDelivery = [
-  //   { buttonTitle: "Retirada", imageSource: <IconMapPin className="size-4" /> },
-  //   { buttonTitle: "Entrega", imageSource: <IconMapPin className="size-4" /> },
-  // ]
+  function handleChange(currentSliderValue) {
+    setSearchParams((prev) => {
+      const currentParams = new URLSearchParams(prev) // Mantém os parâmetros atuais
+      const existingValues = currentParams.get("preco")
+      console.log(existingValues)
+      if (existingValues) {
+        if (currentSliderValue === 0) {
+          currentParams.delete("preco")
+        } else {
+          currentParams.set("preco", String(currentSliderValue))
+        }
+      } else {
+        if (currentSliderValue > 0) {
+          currentParams.set("preco", String(currentSliderValue))
+        }
+      }
+      console.log(currentParams)
+
+      return currentParams // Retorna os parâmetros atualizados
+    })
+  }
 
   const categories = [
-    { buttonTitle: "Salgado", imageSource: <IconMapPin className="size-4" /> },
-    { buttonTitle: "Doce", imageSource: <IconMapPin className="size-4" /> },
-    // { buttonTitle: "Misto", imageSource: <IconMapPin className="size-4" /> },
+    { buttonTitle: "Salgado", imageSource: <IconSalt className="size-4" /> },
+    { buttonTitle: "Doce", imageSource: <IconCandy className="size-4" /> },
   ]
 
   return (
@@ -38,12 +75,16 @@ export function RestaurantFilter() {
       <div className="flex flex-col">
         <h2 className="text-lg font-medium">Filtrar por</h2>
         <div>
-          {/* <FilterItemContainer title={`Ordenar por`} items={ordinateBy} /> */}
-          {/* <FilterItemContainer
-            title={`Tipo de entrega`}
-            items={kindOfDelivery}
-          /> */}
-          <FilterItemContainer title={`Categoria`} items={categories} />
+          <FilterItemContainer
+            title={`Tipo de estabelecimento`}
+            items={storeType}
+            param="tipo"
+          />
+          <FilterItemContainer
+            title={`Categoria`}
+            items={categories}
+            param="categoria"
+          />
         </div>
 
         <PopoverClose className="absolute right-0 top-0 flex justify-center">
@@ -54,12 +95,14 @@ export function RestaurantFilter() {
       <div className="mt-4 flex flex-col gap-3">
         <h2 className="text-sm font-semibold">Preço (R$)</h2>
         <Slider
+          className="px-2"
           defaultValue={sliderValue}
           max={100}
           step={10}
-          onValueChange={(currentSliderValue) =>
+          onValueChange={(currentSliderValue) => {
             setSlidervalue(currentSliderValue)
-          }
+            handleChange(currentSliderValue)
+          }}
         />
         <SliderMetric
           initialValue={0}
