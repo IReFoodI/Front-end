61bb6dcfa5e1316d13ea5925dc589b00f197b98c
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-10-31 09:03:51 -0300

    feat: :sparkles: Initial searchPage

--- src/ui/components/header/search/searchInput.jsx
+++ src/ui/components/header/search/searchInput.jsx
@@ -1,43 +1,74 @@
+import { zodResolver } from "@hookform/resolvers/zod"
 import { IconFilter, IconLoader2, IconSearch } from "@tabler/icons-react"
-import { useEffect, useState } from "react"
+import { useForm } from "react-hook-form"
+import { useNavigate } from "react-router-dom"
+import { toast } from "sonner"
 
-import { useDebounce } from "@/app/hooks/useDebounce"
+import { searchSchema } from "@/domains/user/models/SearchTypes"
 
+import { Form, FormControl, FormField, FormItem } from "../../ui/form/form"
 import { Input } from "../../ui/input"
 import { Popover, PopoverContent, PopoverTrigger } from "../../ui/popover"
 import { RestaurantFilter } from "../restaurantFilter/RestaurantFilter"
 
 export function SearchInput() {
-  const [searchText, setSearchText] = useState("")
+  const navigate = useNavigate()
+  const form = useForm({
+    resolver: zodResolver(searchSchema),
+    defaultValues: {
+      search: "",
+    },
+  })
 
-  const debounceSearchText = useDebounce(searchText)
-
-  function handleUpdateSearchText(e) {
-    setSearchText(e?.target?.value)
+  const onError = (errors) => {
+    if (errors.search) {
+      toast.error(errors.search.message) // Mostra a mensagem de erro em um toast
+    }
   }
-  const loading = false
 
-  useEffect(() => {
-    console.log(debounceSearchText)
-  }, [debounceSearchText])
+  const onSubmit = (data) => {
+    console.log(data)
+
+    navigate(`/produtos/pesquisar?filtrar=${data?.search}`)
+  }
+
+  const loading = false
 
   return (
     <div className="relative order-2 m-auto flex w-full items-center justify-center md:order-1 xl:w-[50%]">
-      {loading ? (
-        <IconLoader2
-          className="absolute left-0 ml-2 animate-spin text-primary"
-          size={20}
-        />
-      ) : (
-        <IconSearch className="absolute left-0 ml-2 text-primary" size={20} />
-      )}
-      <Input
-        type="text"
-        placeholder="Busque por estabelecimentos"
-        className="bg-background pl-8"
-        value={searchText}
-        onChange={handleUpdateSearchText}
-      />
+      <Form {...form} className="flex w-full">
+        <form
+          onSubmit={form.handleSubmit(onSubmit, onError)}
+          className="flex w-full items-center"
+        >
+          {loading ? (
+            <IconLoader2
+              className="absolute left-0 ml-2 animate-spin text-primary"
+              size={20}
+            />
+          ) : (
+            <button className="absolute left-0 ml-2">
+              <IconSearch className="text-primary" size={20} />
+            </button>
+          )}
+          <FormField
+            control={form.control}
+            name="search"
+            render={({ field }) => (
+              <FormItem className="flex w-full">
+                <FormControl>
+                  <Input
+                    {...field}
+                    type="text"
+                    placeholder="Busque por produtos"
+                    className="bg-background pl-8"
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
 
       <Popover className="relative" placement="center">
         <PopoverTrigger className="absolute right-0 mr-2 text-gray-500">
