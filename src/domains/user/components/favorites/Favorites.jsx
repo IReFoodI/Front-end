38f215e212ffa3fc503223282b9a0fe13776f74a
Felipe Fracasso <93887208+FelipeM-F@users.noreply.github.com>
2024-11-01 09:32:22 -0400

    style: :lipstick: Ajustes na responsividade

--- src/domains/user/components/favorites/Favorites.jsx
+++ src/domains/user/components/favorites/Favorites.jsx
@@ -12,7 +12,7 @@ export function Favorites() {
         Favoritos
       </h1>
       <div className="flex h-full pt-24">
-        <div className="mx-auto lg:mx-20">
+        <div className="mx-auto custom:mx-12 md:mx-24 lg:mx-0 xl:mx-20">
           {loading ? (
             <p>Loading...</p>
           ) : (
