48dd4029a24d34c5d85439202e422f7769a2f1af
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-11-01 19:19:59 -0300

    feat: integra back no order items

--- src/domains/user/components/finalizeOrderPage/Orderitems.jsx
+++ src/domains/user/components/finalizeOrderPage/Orderitems.jsx
@@ -3,88 +3,46 @@ import { useState } from "react"
 import { Link } from "react-router-dom"
 
 import { currencyFormatter } from "@/app/utils/currencyFormatter"
+import useCartStore from "@/ui/components/header/Cart/useCartStore"
 
 const Orderitems = () => {
   const [showAllItems, setShowAllItems] = useState(false)
 
+  const { cartItems, subtotal, removeItemFromCart } = useCartStore()
+
   const toggleShowAllItems = () => {
     setShowAllItems((prevState) => !prevState)
   }
-  const products = [
-    {
-      id: 1,
-      fullPrice: 60,
-      price: 40.020202,
-      name: "Combo Hamburguer",
-      quantity: 3,
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    },
-    {
-      id: 2,
-      fullPrice: 40,
-      price: 20,
-      name: "Misto de Queijo",
-      quantity: 5,
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elitrunt totam sed.",
-    },
-    {
-      id: 3,
-      fullPrice: 40,
-      price: 20,
-      name: "Subway",
-      quantity: 15,
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elitrunt totam sed.",
-    },
-    {
-      id: 4,
-      fullPrice: 40,
-      price: 16,
-      name: "Hotdog",
-      quantity: 8,
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elitrunt totam sed.",
-    },
-    {
-      id: 3,
-      fullPrice: 40,
-      price: 20,
-      name: "Beirute",
-      quantity: 2,
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elitrunt totam sed.",
-    },
-  ]
 
-  let totalAmount = 151.79
-
-  const itemsToDisplay = showAllItems ? products : products.slice(0, 2)
+  const itemsToDisplay = showAllItems ? cartItems : cartItems.slice(0, 2)
 
   return (
     <aside className="mx-auto flex max-w-[400px] flex-1 flex-col items-center gap-4 rounded-md border p-8 shadow-lg lg:gap-6">
       <div className="flex flex-1 flex-col gap-1">
         {/* CARD PRODUTO ITEM */}
         <div className="flex flex-col gap-4">
-          {itemsToDisplay.map((product) => (
+          {itemsToDisplay.map((item) => (
             <div
-              key={product.id}
+              key={item.productId}
               className="flex items-center justify-between rounded-md bg-secondary p-2 text-sm"
             >
               <div className="flex w-full items-center justify-between gap-5">
                 <div className="flex flex-1 items-center justify-between">
                   <div>
                     <p className="text-sm font-bold lg:text-lg">
-                      {product.quantity}x {product.name}
+                      {item.quantity}x {item.nameProduct}
                     </p>
-                    <p>{product.description}</p>
+                    <p>{item.descriptionProduct}</p>
                   </div>
                   <span className="text-left text-xs font-semibold lg:text-base">
-                    {currencyFormatter(product.price)}
+                    {currencyFormatter(item.unitValue)}
                   </span>
                 </div>
                 <div>
-                  <IconTrash className="cursor-pointer" />
+                  <IconTrash
+                    className="cursor-pointer"
+                    onClick={() => removeItemFromCart(item.productId)}
+                  />
                 </div>
               </div>
             </div>
@@ -92,7 +50,7 @@ const Orderitems = () => {
         </div>
       </div>
       {/* VER MAIS */}
-      {products.length > 2 && (
+      {cartItems.length > 2 && (
         <button onClick={toggleShowAllItems} className="mt-2 text-primary">
           {showAllItems ? "Ver menos itens" : "Ver mais itens"}
         </button>
@@ -106,9 +64,7 @@ const Orderitems = () => {
       </Link>
       {/* TOTAL */}
       <div className="p-4 text-center lg:text-right">
-        <p className="text-lg font-semibold">
-          Total: R$ {totalAmount.toFixed(2)}
-        </p>
+        <p className="text-lg font-semibold">Total: R$ {subtotal}</p>
       </div>
     </aside>
   )
