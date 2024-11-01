53704ba548b7d739109bca265aad6a8a7cadf2af
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-11-01 09:09:44 -0300

    refactor: :recycle: search service now tranform filters to uppercase

--- src/domains/user/services/searchProductsService.js
+++ src/domains/user/services/searchProductsService.js
@@ -4,7 +4,18 @@ const axios = createAxiosInstance(true)
 
 async function searchProducts(searchParams) {
   console.log(searchParams)
-  return await axios.get(`${BASE_URL}?${searchParams}`)
+  let transformed = searchParams?.replace(
+    /(tipo=)([^&]*)|(categoria=)([^&]*)/g,
+    (match, tipo, tipoValue, categoria, categoriaValue) => {
+      if (tipo) {
+        return tipo + tipoValue.toUpperCase() // Transforma o valor de tipo em uppercase
+      } else if (categoria) {
+        return categoria + categoriaValue.toUpperCase() // Transforma o valor de categoria em uppercase
+      }
+      return match
+    }
+  )
+  return await axios.get(`${BASE_URL}?${transformed}`)
 }
 
 export { searchProducts }
