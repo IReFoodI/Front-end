e950ec0775d2e8c90af8eb2e2c615cd54f51a8ba
Samilis Brito <samilisbritto@gmail.com>
2024-11-06 23:45:30 -0300

    ✨ feat: add contact number mask in BasicUserInfo

--- src/ui/components/myDataPage/BasicUserInfo.jsx
+++ src/ui/components/myDataPage/BasicUserInfo.jsx
@@ -1,13 +1,24 @@
 import { Label } from "@radix-ui/react-label"
+import { PatternFormat } from "react-number-format"
 
 export function BasicUserInfo({ label, data }) {
   return (
-    <div className="mt-4 flex w-full flex-col rounded-xl lg:bg-gray-50">
+    <div className="mt-4 flex w-full flex-col rounded-xl lg:bg-secondary">
       <Label className="text-semibold bottom-3 left-3 text-xs text-gray-400 lg:relative lg:text-base lg:font-semibold lg:text-gray-600">
         {label}
       </Label>
       <p className="bottom-3 text-lg font-semibold text-gray-600 lg:relative lg:mx-1 lg:p-1 lg:font-medium lg:text-gray-500">
-        {data}
+        {label === "Contato" ? (
+          <PatternFormat
+            value={data}
+            displayType="text"
+            format="(##) #####-####"
+            allowEmptyFormatting
+            mask="_"
+          />
+        ) : (
+          data
+        )}
       </p>
     </div>
   )
