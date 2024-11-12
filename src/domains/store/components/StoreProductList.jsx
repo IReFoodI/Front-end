88f7ec4395d7d79beb617c5bf376ed9250ea37d5
GenaraSousa <genara7sousa@gmail.com>
2024-11-12 18:50:42 -0300

    fix: responsividade da grid de produtos no tamanho 1024+

--- src/domains/store/components/StoreProductList.jsx
+++ src/domains/store/components/StoreProductList.jsx
@@ -49,7 +49,7 @@ export function StoreProductList() {
           </SelectContent>
         </Select>
       </div>
-      <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 xl:grid-cols-3">
         {products?.map((product, index) => (
           <StoreProductItem {...product} key={product.productId + index} />
         ))}
