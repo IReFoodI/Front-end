d7f2696d18164e448316fd812b63ace0dd61a9c6
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-12-19 04:18:45 -0300

    chore: :wrench: Add svg config to jest.config

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,8 @@ export default {
   },
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest", // Certifique-se de que o Jest sabe lidar com JSX/TSX
+    "^.+\\.svg$": "<rootDir>/svgTransform.js",
   },
-  setupFilesAfterEnv: ["@testing-library/jest-dom"], // Configura jest-dom
+  setupFilesAfterEnv: ["@testing-library/jest-dom", "<rootDir>/jest.setup.js"], // Configura jest-dom
   testMatch: ["**/src/tests/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"], // Detecta arquivos de teste
 }
