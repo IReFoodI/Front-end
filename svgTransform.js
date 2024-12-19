b49695986ad2af7c55867a91bcd825debb149388
Caio Martins Mendes Rodrigues <115363966+CaioMMendes@users.noreply.github.com>
2024-12-19 04:16:51 -0300

    chore: :wrench: Add svg transform test config

---
+++ svgTransform.js
@@ -0,0 +1,12 @@
+import path from "path"
+
+export default {
+  process(sourcePath) {
+    return {
+      code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
+    }
+  },
+  getCacheKey() {
+    return "svgTransform"
+  },
+}
